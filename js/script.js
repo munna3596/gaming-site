7e7347b37d2fb818146515985bc747788eabd57f
Your Name01~ <you@example.com>
2023-05-18 09:31:24 -0700

    project done

---
+++ js/script.js
@@ -0,0 +1,18 @@
+document.getElementById("sign-up-btn").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "flex";
+});
+document.getElementById("sign-up-button").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "flex";
+});
+document.getElementById("close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+});
+document.getElementById("sign-in-btn").addEventListener("click", function(){
+    document.querySelector(".popup-n").style.display = "flex";
+});
+document.getElementById("sign-in-button").addEventListener("click", function(){
+    document.querySelector(".popup-n").style.display = "flex";
+});
+document.getElementById("closes").addEventListener("click", function(){
+    document.querySelector(".popup-n").style.display = "none";
+});
